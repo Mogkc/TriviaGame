411f70102922ce9f5439fb8d13f4c329802238eb
Mogkc <carson.mogk@cox.net>
2019-03-02 23:55:51 -0800

    all but the timer resetting

---
+++ assets/javascript/app.js
@@ -0,0 +1,101 @@
+var questions = [
+    { q: "What is the Trivium?", a: "Medieval college curiculum", b: "Something that doesn't matter", c: "Another name for the illuminati", d: "A committee of three people", actual: "a" },
+    { q: "What doesn't the Trivium cover?", a: "Logic", b: "Grammar", c: "Astronomy", d: "Rhetoric", actual: "c" },
+    { q: "Why is the Trivium important?", a: "It isn't", b: "It teaches how to identify bad faith arguments", c: "English teachers care about it", d: "It's a prerequisite for math classes", actual: "b" }
+];
+
+var correct;
+var currQues;
+var seconds;
+var timer;
+//Game doesn't start until they click start
+var gameOver = true;
+
+
+var outOfTime = function () {
+    $("#fin").text("Out of Time!");
+    $("#chosen").text("You didn't chose any answer");
+    $("#correct").text("The correct answer was" + $("#" + questions[currQues].actual).text());
+    show();
+
+    nextQuestion();
+}
+
+var newGame = function () {
+    //Reset values
+    gameOver = false;
+    correct = 0;
+    currQues = -1;
+    //Start the first question
+    nextQuestion();
+    hide();
+}
+
+var nextQuestion = function () {
+    currQues++;
+    if (currQues < questions.length) {
+        //Display next question
+        $("#question").text(questions[currQues].q);
+        $("#a").text(questions[currQues].a);
+        $("#b").text(questions[currQues].b);
+        $("#c").text(questions[currQues].c);
+        $("#d").text(questions[currQues].d);
+        //Start question timer from max value
+        seconds = 31;
+        countDown();
+    } else {
+        gameOver = true;
+        prepFinal();
+        show();
+    }
+}
+
+var countDown = function () {
+    seconds--;
+    if (seconds > 0) {
+        timer = setInterval(countDown, 1000);
+        $("#time").text(seconds);
+    } else {
+        outOfTIme();
+    }
+}
+
+var prepFinal = function () {
+    $("#fin").text("Game Over!");
+    $("#chosen").text("You got " + correct + " questions right!");
+    $("#correct").text("There were " + questions.length + " questions total.");
+}
+
+var show = function () {
+    $("#results").css("z-index", 1);
+    if (gameOver) {
+        $("#new-game").css("display", "block");
+    }
+    timer = setInterval(hide, 5000);
+}
+
+var hide = function () {
+    if (!gameOver) {
+        $("#results").css("z-index", -1);
+    }
+}
+
+$(".ans").on("click", function (event) {
+    clearInterval(timer);
+    var ans = questions[currQues].actual;
+    if (event.target.id === ans) {
+        $("#fin").text("Correct!");
+    } else {
+        $("#fin").text("Incorrect");
+    }
+    $("#chosen").text("You chose " + $(event.target).text());
+    $("#correct").text("The correct answer was: " + $("#" + ans).text());
+    show();
+
+    nextQuestion();
+});
+
+$("#new-game").on("click", function () {
+    newGame();
+    $("#new-game").css("display", "none");
+})
